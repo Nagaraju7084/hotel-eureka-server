9ddf921aca4788ef510e2470b64cd12fb62fe43d
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2023-10-24 17:46:48 +0530

    Initial commit

---
+++ src/test/java/com/boot/hotel/eureka/HotelEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.hotel.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
